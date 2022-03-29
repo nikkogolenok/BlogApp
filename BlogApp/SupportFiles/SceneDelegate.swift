ee834db9eb9b1d2b8e5a5702962daf0c43c59755
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-29 16:15:47 +0300

    Delete Storyboard and all information about it And create new window view

--- BlogApp/SupportFiles/SceneDelegate.swift
+++ BlogApp/SupportFiles/SceneDelegate.swift
@@ -12,7 +12,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        
+        let vc = ViewController()
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
